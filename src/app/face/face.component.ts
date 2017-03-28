ed88888d8e0d98effc7417c1c4c1cfdb8c3b6132
Jesse Pinho <jesse@jessepinho.com>
2017-03-28 21:33:04 +0200

    Install Color; use it to manipulate the color of the square

--- src/app/face/face.component.ts
+++ src/app/face/face.component.ts
@@ -1,18 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import * as Color from 'color';
+
+const INITIAL_COLOR: string = 'rgb(222, 140, 93)';
+const RED: string = 'rgb(255, 0, 0)';
 
 @Component({
   selector: 'app-face',
   templateUrl: './face.component.html',
   styleUrls: ['./face.component.sass']
 })
-export class FaceComponent implements OnInit {
+export class FaceComponent implements OnChanges {
   @Input() private intensity: number;
 
-  private color: string = 'rgb(222, 140, 93)';
+  private color: string = INITIAL_COLOR;
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.hasOwnProperty('intensity')) {
+      this.color = Color(INITIAL_COLOR).mix(Color(RED), this.intensity);
+    }
   }
-
 }
