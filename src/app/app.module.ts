ff0d857a22c19bbb3b613df4572d3fcf2109052b
Jesse Pinho <jesse@jessepinho.com>
2017-03-29 01:11:49 +0200

    Show a face

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { InlineSVGModule } from 'ng-inline-svg';
 
 import { AppComponent } from './app.component';
 import { FaceComponent } from './face/face.component';
@@ -19,7 +20,8 @@ import { TwitterService } from './twitter.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    InlineSVGModule,
   ],
   providers: [
     TwitterService,
