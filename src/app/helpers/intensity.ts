d0ad68cc678a965583e9342a1b5b7e4b798e5338
Jesse Pinho <jesse@jessepinho.com>
2017-03-28 23:47:35 +0200

    Auto-generated commit

--- src/app/helpers/intensity.ts
+++
@@ -1,6 +0,0 @@
-export function intensity(tweet: string): number {
-  const { length } = tweet.replace(/\s/g, '');
-  const intenseCharacters = (tweet.match(/[A-Z\!]/g) || []).length;
-
-  return intenseCharacters / length;
-}
