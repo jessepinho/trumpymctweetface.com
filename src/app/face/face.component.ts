dce1fa6342bc6b37621da5e450932e514d6ec568
Jesse Pinho <jesse@jessepinho.com>
2017-03-28 23:25:12 +0200

    Exaggerate the colors (for now)

--- src/app/face/face.component.ts
+++ src/app/face/face.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import * as Color from 'color';
 
-const INITIAL_COLOR: string = 'rgb(222, 140, 93)';
-const RED: string = 'rgb(255, 0, 0)';
+const INITIAL_COLOR: string = 'rgb(0, 0, 0)';
+const INTENSE_COLOR: string = 'rgb(255, 255, 255)';
 
 @Component({
   selector: 'app-face',
@@ -16,7 +16,7 @@ export class FaceComponent implements OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.hasOwnProperty('intensity')) {
-      this.color = Color(INITIAL_COLOR).mix(Color(RED), this.intensity);
+      this.color = Color(INITIAL_COLOR).mix(Color(INTENSE_COLOR), this.intensity);
     }
   }
 }
