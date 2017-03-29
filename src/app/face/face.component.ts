d74c92795cd887b0048165926877d2fd6c086112
Jesse Pinho <jesse@jessepinho.com>
2017-03-29 23:34:03 +0200

    Use D3 to manipulate face color

--- src/app/face/face.component.ts
+++ src/app/face/face.component.ts
@@ -1,8 +1,12 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import * as Color from 'color';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import * as d3 from 'd3';
 
-const INITIAL_COLOR: string = 'rgb(0, 0, 0)';
-const INTENSE_COLOR: string = 'rgb(255, 255, 255)';
+import { SvgComponent } from '../svg/svg.component';
 
 @Component({
   selector: 'app-face',
@@ -11,12 +15,22 @@ const INTENSE_COLOR: string = 'rgb(255, 255, 255)';
 })
 export class FaceComponent implements OnChanges {
   @Input() private intensity: number;
-
-  private color: string = INITIAL_COLOR;
+  private selection;
+  private color = d3
+    .scaleLinear<string>()
+    .domain([0, 1])
+    .range(['#edb48e', '#ff6f00'])
+    .interpolate(d3.interpolateHcl);
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes.hasOwnProperty('intensity')) {
-      this.color = Color(INITIAL_COLOR).mix(Color(INTENSE_COLOR), this.intensity);
+    if (changes.hasOwnProperty('intensity') && this.selection) {
+      this.selection
+        .data([this.intensity])
+        .attr('fill', d => this.color(d));
     }
+  }
+
+  private handleSVGLoaded(svg: SVGSVGElement): void {
+    this.selection = d3.select(svg).select('#face');
   }
 }
--- src/app/svg/svg.component.ts
+++ src/app/svg/svg.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-svg',
   templateUrl: './svg.component.html',
   styleUrls: ['./svg.component.css']
 })
-export class SvgComponent implements OnInit {
+export class SvgComponent implements AfterViewInit {
+  @Output() private svgLoaded: EventEmitter<SVGSVGElement> = new EventEmitter<SVGSVGElement>();
 
-  constructor() { }
+  constructor(
+    private element: ElementRef
+  ) {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    const svg: SVGSVGElement = this.element.nativeElement.getElementsByTagName('svg')[0];
+    this.svgLoaded.emit(svg);
   }
-
 }
