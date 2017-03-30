76ce8ea4be6d2372a05b98881b1a2d0c059ae4f7
Jesse Pinho <jesse@jessepinho.com>
2017-03-30 19:53:50 +0200

    Make his mouth move

--- src/app/face/face.component.ts
+++ src/app/face/face.component.ts
@@ -18,24 +18,50 @@ const NEXT_LEVEL_SKIN_TONE = '#ff6f00';
 })
 export class FaceComponent implements OnChanges {
   @Input() private intensity: number;
+
+  private face;
+  private mouthTop;
+  private mouthBottom;
   private selection;
-  private color = d3
+
+  private faceColor = d3
     .scaleLinear<string>()
     .domain([0, 1])
     .range([NORMAL_SKIN_TONE, NEXT_LEVEL_SKIN_TONE])
     .interpolate(d3.interpolateHcl);
 
+  private mouthOpening = d3
+    .scaleLinear<number>()
+    .domain([0, 1])
+    .range([0, 30]);
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.hasOwnProperty('intensity') && this.selection) {
-      this.selection
+      this.face
         .data([this.intensity])
         .transition()
         .duration(250)
-        .attr('fill', d => this.color(d));
+        .attr('fill', d => this.faceColor(d));
+
+      this.mouthTop
+        .data([this.intensity])
+        .transition()
+        .duration(250)
+        .attr('transform', d => `translate(0, -${this.mouthOpening(d)})`);
+
+      this.mouthBottom
+        .data([this.intensity])
+        .transition()
+        .duration(250)
+        .attr('transform', d => `translate(0, ${this.mouthOpening(d)})`);
     }
   }
 
   private handleSVGLoaded(svg: SVGSVGElement): void {
-    this.selection = d3.select(svg).select('#face');
+    this.selection = d3.select(svg);
+
+    this.face = this.selection.select('#face');
+    this.mouthTop = this.selection.select('#mouthTop');
+    this.mouthBottom = this.selection.select('#mouthBottom');
   }
 }
