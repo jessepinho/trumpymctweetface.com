26ab9d9eb2a59dea99178ca554fbf0745fd953e9
Jesse Pinho <jesse@jessepinho.com>
2017-04-27 23:03:50 +0200

    feat: use the square root to get a wider range of intensities

--- src/app/helpers/intensity.ts
+++ src/app/helpers/intensity.ts
@@ -2,5 +2,6 @@ export function intensity(tweet: string): number {
   const { length } = tweet.replace(/\s/g, '');
   const intenseCharacters = (tweet.match(/[A-Z\!]/g) || []).length;
 
-  return intenseCharacters / length;
+  // Use the square root to get a wider range of intensities.
+  return Math.sqrt(intenseCharacters / length);
 }
