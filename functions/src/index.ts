28f5daa0d798fffb0f1266e77615cd6d8aab612a
Jesse Pinho <jesse@jessepinho.com>
2018-03-19 07:26:18 +0100

    feat: copy over the API function and install dependencies

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,36 @@
+import * as express from 'express';
 import * as functions from 'firebase-functions';
+import * as httpProxy from 'http-proxy';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+const proxy = httpProxy
+  .createProxyServer({
+    changeOrigin: true,
+    headers: {
+      Authorization: `Bearer ${functions.config().twitter.token}`,
+    },
+    target: 'https://api.twitter.com',
+  });
+
+proxy.on('proxyRes', function(proxyRes, req, res) {
+  if (process.env.NODE_ENV === 'development') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', 'http://trumpymctweetface.com');
+  }
+});
+
+app.get('/1.1/statuses/user_timeline.json', function(req, res) {
+  if (req.query.screen_name === 'realDonaldTrump') {
+    proxy.web(req, res);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.get('/1.1/statuses/show.json', function(req, res) {
+  proxy.web(req, res);
+});
+
+export const api = functions.https.onRequest(app);
