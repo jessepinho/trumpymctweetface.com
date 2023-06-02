2393e71fe92a01364a87a25801348738e626ef6c
Jesse Pinho <jesse@jessepinho.com>
2023-06-01 17:13:45 -0700

    Extract helper function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,41 +1,49 @@
-import * as express from 'express';
-import * as functions from 'firebase-functions';
-import * as httpProxy from 'http-proxy';
+import * as express from "express";
+import * as functions from "firebase-functions";
+import * as httpProxy from "http-proxy";
 
 const app = express();
 const proxyRouter = express.Router();
 
-const proxy = httpProxy
-  .createProxyServer({
-    changeOrigin: true,
-    headers: {
-      Authorization: `Bearer ${functions.config().twitter.token}`,
-    },
-    target: 'https://api.twitter.com',
-  });
-
-proxy.on('proxyRes', function(proxyRes, req, res) {
-  if (process.env.NODE_ENV === 'development') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-  } else {
-    res.setHeader('Access-Control-Allow-Origin', 'https://trumpymctweetface.jessepinho.com');
-  }
+const proxy = httpProxy.createProxyServer({
+  changeOrigin: true,
+  headers: {
+    Authorization: `Bearer ${functions.config().twitter.token}`,
+  },
+  target: "https://api.twitter.com",
 });
 
-proxyRouter.get('/1.1/statuses/user_timeline.json', function(req, res) {
-  if (req.query.screen_name === 'realDonaldTrump') {
+const setAccessControlAllowOriginHeader = (res: express.Response) => {
+  if (process.env.NODE_ENV === "development") {
+    // TODO: Fix typings
+    // @ts-ignore
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  } else {
+    // TODO: Fix typings
+    // @ts-ignore
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://trumpymctweetface.jessepinho.com"
+    );
+  }
+};
+
+proxyRouter.get("/1.1/statuses/user_timeline.json", function (req, res) {
+  setAccessControlAllowOriginHeader(res);
+
+  if (req.query.screen_name === "realDonaldTrump") {
     proxy.web(req, res);
   } else {
     res.sendStatus(403);
   }
 });
 
-proxyRouter.get('/1.1/statuses/show.json', function(req, res) {
+proxyRouter.get("/1.1/statuses/show.json", function (req, res) {
   proxy.web(req, res);
 });
 
-app.use('/api', proxyRouter);
+app.use("/api", proxyRouter);
 
-// TODO: Get typings to work here
+// TODO: Fix typings
 // @ts-ignore
 export const api = functions.https.onRequest(app);
